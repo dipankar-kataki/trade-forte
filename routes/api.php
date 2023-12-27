e1c733dbcd70674e42bd505c4c03542578c214a7
jay-ekodus <jsardar@ekodusinc.com>
2023-12-27 15:00:18 +0530

    changed api routes

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::post('resetpassword', [UserController::class, 'resetPassword'])->name('re
 Route::group(["middleware" => 'auth:sanctum'], function () {
     Route::group(['prefix' => 'users'], function () {
         Route::post('create', [UserController::class, 'create']);
-        Route::get('get/list', [UserController::class, 'index']);
+        Route::get('list', [UserController::class, 'index']);
         Route::get('get/{id}', [UserController::class, 'show']);
         Route::put('update/{id}', [UserController::class, 'update']);
         Route::delete('delete/{id}', [UserController::class, 'destroy']);
@@ -43,88 +43,88 @@ Route::group(["middleware" => 'auth:sanctum'], function () {
     });
     Route::group(['prefix' => 'exporters'], function () {
         Route::post('create', [ExporterController::class, 'create']);
-        Route::get('get/list', [ExporterController::class, 'index']);
+        Route::get('list', [ExporterController::class, 'index']);
         Route::get('get/{id}', [ExporterController::class, 'show']);
         Route::put('update/{exporterId}', [ExporterController::class, 'update']);
         Route::delete('delete/{exporterId}', [ExporterController::class, 'destroy']);
     });
     Route::group(['prefix' => 'bankaccount'], function () {
         Route::post('create', [BankAccountController::class, 'create']);
-        Route::get('get/list', [BankAccountController::class, 'index']);
+        Route::get('list', [BankAccountController::class, 'index']);
         Route::get('get/{id}', [BankAccountController::class, 'show']);
         Route::put('update/{id}', [BankAccountController::class, 'update']);
         Route::delete('delete/{id}', [BankAccountController::class, 'destroy']);
     });
     Route::group(['prefix' => 'consignees'], function () {
         Route::post('create', [ConsigneeController::class, 'create']);
-        Route::get('get/list', [ConsigneeController::class, 'index']);
+        Route::get('list', [ConsigneeController::class, 'index']);
         Route::get('get/{id}', [ConsigneeController::class, 'show']);
         Route::put('update/{id}', [ConsigneeController::class, 'update']);
         Route::delete('delete/{id}', [ConsigneeController::class, 'destroy']);
     });
     Route::group(['prefix' => 'country'], function () {
         Route::post('create', [CountryController::class, 'create']);
-        Route::get('get/list', [CountryController::class, 'index']);
+        Route::get('list', [CountryController::class, 'index']);
         Route::delete('delete/{id}', [CountryController::class, 'destroy']);
     });
     Route::group(['prefix' => 'declarations'], function () {
         Route::post('create', [DeclarationController::class, 'create']);
-        Route::get('get/list', [DeclarationController::class, 'index']);
+        Route::get('list', [DeclarationController::class, 'index']);
         Route::get('get/{id}', [DeclarationController::class, 'show']);
         Route::put('update/{id}', [DeclarationController::class, 'update']);
         Route::delete('delete/{id}', [DeclarationController::class, 'destroy']);
     });
     Route::group(['prefix' => 'invoice'], function () {
         Route::post('create', [InvoiceController::class, 'create']);
-        Route::get('get/list', [InvoiceController::class, 'index']);
+        Route::get('list', [InvoiceController::class, 'index']);
         Route::get('get/{id}', [InvoiceController::class, 'show']);
         Route::put('update/{id}', [InvoiceController::class, 'update']);
         Route::delete('delete/{id}', [InvoiceController::class, 'destroy']);
     });
     Route::group(['prefix' => 'invoiceitems'], function () {
         Route::post('create', [InvoiceItemsController::class, 'create']);
-        Route::get('get/list', [InvoiceItemsController::class, 'index']);
+        Route::get('list', [InvoiceItemsController::class, 'index']);
         Route::get('get/{id}', [InvoiceItemsController::class, 'show']);
         // Route::put('update/{bankAccountId}', [InvoiceItemsController::class, 'update']);
         // Route::delete('delete/{bankAccountId}', [InvoiceItemsController::class, 'destroy']);
     });
     Route::group(['prefix' => 'modules'], function () {
         Route::post('create', [ModuleController::class, 'create']);
-        Route::get('get/list', [ModuleController::class, 'index']);
+        Route::get('list', [ModuleController::class, 'index']);
         Route::get('get/{id}', [ModuleController::class, 'show']);
         Route::put('update/{moduleId}', [ModuleController::class, 'update']);
         Route::delete('delete/{moduleId}', [ModuleController::class, 'destroy']);
     });
     Route::group(['prefix' => 'packaging'], function () {
         Route::post('create', [PackagingController::class, 'create']);
-        Route::get('get/list', [PackagingController::class, 'index']);
+        Route::get('list', [PackagingController::class, 'index']);
         Route::get('get/{id}', [PackagingController::class, 'show']);
         Route::put('update/{id}', [PackagingController::class, 'update']);
         Route::delete('delete/{id}', [PackagingController::class, 'destroy']);
     });
     Route::group(['prefix' => 'shipping'], function () {
         Route::post('create', [ShippingAddressController::class, 'create']);
-        Route::get('get/list', [ShippingAddressController::class, 'index']);
+        Route::get('list', [ShippingAddressController::class, 'index']);
         Route::get('get/{id}', [ShippingAddressController::class, 'show']);
         Route::put('update/{id}', [ShippingAddressController::class, 'update']);
         Route::delete('delete/{id}', [ShippingAddressController::class, 'destroy']);
     });
     Route::group(['prefix' => 'lorry_details'], function () {
         Route::post('create', [LorryController::class, 'create']);
-        Route::get('get/list', [LorryController::class, 'index']);
+        Route::get('list', [LorryController::class, 'index']);
         Route::get('get/{id}', [LorryController::class, 'show']);
         Route::put('update/{id}', [LorryController::class, 'update']);
         Route::delete('delete/{id}', [LorryController::class, 'destroy']);
     });
     Route::group(['prefix' => 'lorryitems'], function () {
         Route::post('create', [LorryItemsController::class, 'create']);
-        Route::get('get/list', [LorryItemsController::class, 'index']);
+        Route::get('list', [LorryItemsController::class, 'index']);
         Route::get('get/{id}', [LorryItemsController::class, 'show']);
         Route::put('update/{id}', [LorryItemsController::class, 'update']);
         Route::delete('delete/{id}', [LorryItemsController::class, 'destroy']);
     });
     Route::group(['prefix' => 'useractivity'], function () {
-        Route::get('get/list', [UserActivityController::class, 'index']);
+        Route::get('list', [UserActivityController::class, 'index']);
         Route::get('get/{id}', [UserActivityController::class, 'show']);
     });
 });
