f7d88b6e80785e116b285ccb7893d122d5bb53bc
jay-ekodus <jsardar@ekodusinc.com>
2023-12-28 22:24:11 +0530

    changed invoice_id format -4

--- app/Models/InvoiceDetail.php
+++ app/Models/InvoiceDetail.php
@@ -27,7 +27,7 @@ class InvoiceDetail extends Model
             'vehicle_no' => 'nullable|string',
             'insurance' => 'nullable|string',
             'buyer_no' => 'nullable|string',
-            'invoice_date' => 'nullable|date',
+            'invoice_date' => 'nullable|date',  
             'eway_bill_id' => 'nullable|string',
         ];
     }
