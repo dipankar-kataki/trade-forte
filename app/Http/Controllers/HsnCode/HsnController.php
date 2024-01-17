b6049f70aaef04cc53d56e51588d30a77d256668
jay-ekodus <jsardar@ekodusinc.com>
2024-01-17 15:14:44 +0530

    added log-2

--- app/Http/Controllers/HsnCode/HsnController.php
+++ app/Http/Controllers/HsnCode/HsnController.php
@@ -13,17 +13,22 @@ class HsnController extends Controller
     use ApiResponse;
     public function list(Request $request)
     {
-        $searchTerm = $request->hsn;
-        var_dump($searchTerm);
-        $searchResults = HsnTable::selectRaw(
-            "*, MATCH(hsn_code, hsn_description) AGAINST(? IN BOOLEAN MODE) as relevance",
-            [$searchTerm]
-        )
-            ->whereRaw("MATCH(hsn_code, hsn_description) AGAINST(? IN BOOLEAN MODE)", [$searchTerm])
-            ->orderByDesc('relevance')
-            ->limit(5)
-            ->get();
-        Log::info("Fetched hsn list");
-        return $this->success("Hsn List.", $searchResults, null, 200);
+        try {
+            $searchTerm = $request->hsn;
+            var_dump($searchTerm);
+            $searchResults = HsnTable::selectRaw(
+                "*, MATCH(hsn_code, hsn_description) AGAINST(? IN BOOLEAN MODE) as relevance",
+                [$searchTerm]
+            )
+                ->whereRaw("MATCH(hsn_code, hsn_description) AGAINST(? IN BOOLEAN MODE)", [$searchTerm])
+                ->orderByDesc('relevance')
+                ->limit(5)
+                ->get();
+            Log::info("Fetched hsn list");
+            return $this->success("Hsn List.", $searchResults, null, 200);
+        } catch (\Exception $e) {
+            return $this->error('Oops! Something Went Wrong.' . $e->getMessage(), null, null, 500);
+        }
+
     }
 }
