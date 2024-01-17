725fa8d8b589e7aa7e156d0961ecd1dec094b9ce
jay-ekodus <jsardar@ekodusinc.com>
2024-01-17 15:30:55 +0530

    changed list

--- app/Http/Controllers/Exporters/ExporterController.php
+++ app/Http/Controllers/Exporters/ExporterController.php
@@ -46,7 +46,7 @@ class ExporterController extends Controller
     public function index(Request $request)
     {
         try {
-            $exporter = Exporter::select('id', 'name', 'phone')->get();
+            $exporter = Exporter::select('id', 'name', 'phone', "address")->get();
             return $this->success("Exporter list.", $exporter, null, 200);
         } catch (\Exception $e) {
             return $this->error('Oops! Something Went Wrong.' . $e->getMessage(), null, null, 500);
