4f906d56ecc232a741bab88dafea01449c295e96
jay-ekodus <jsardar@ekodusinc.com>
2024-02-03 14:24:50 +0530

    added bank account consignee-6

--- app/Http/Controllers/ConsigneeBank/ConsigneesBankController.php
+++ app/Http/Controllers/ConsigneeBank/ConsigneesBankController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\BankAccounts;
+namespace App\Http\Controllers\ConsigneesBankController;
 
 use App\Http\Controllers\Controller;
 use App\Models\ConsigneeBankModel;
