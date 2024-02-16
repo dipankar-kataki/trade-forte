1b6c1bede1cbddeceb6c56402eea51826524a454
jay-ekodus <jsardar@ekodusinc.com>
2024-02-16 17:13:38 +0530

    invoice model update-92

--- app/Http/Controllers/HsnCode/HsnController.php
+++ app/Http/Controllers/HsnCode/HsnController.php
@@ -15,12 +15,19 @@ class HsnController extends Controller
     {
         try {
             $searchTerm = $request->hsn;
-            $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
-                ->orderBy('hsn_code')
-                ->limit(8)
-                ->get();
-            // Log::info("Fetched hsn list");
-            return $this->success("Hsn List.", $searchResults, null, 200);
+            $length = strlen($searchTerm);
+
+            // Validate the length of the HSN code in the request
+            if ($length % 2 == 0 && $length >= 2 && $length <= 8) {
+                $searchResults = HsnTable::where('hsn_code', 'LIKE', substr($searchTerm, 0, $length) . '%')
+                    ->orderBy('hsn_code')
+                    ->limit(8)
+                    ->get();
+
+                return $this->success("Hsn List.", $searchResults, null, 200);
+            } else {
+                return $this->error('Invalid HSN code length in the request.', null, null, 400);
+            }
         } catch (\Exception $e) {
             return $this->error('Oops! Something Went Wrong.' . $e->getMessage(), null, null, 500);
         }
