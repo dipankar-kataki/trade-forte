342704a2d5a21adb96d84b7e24b73cf23f9653ba
jay-ekodus <jsardar@ekodusinc.com>
2024-02-16 17:29:00 +0530

    invoice model update-97

--- app/Http/Controllers/HsnCode/HsnController.php
+++ app/Http/Controllers/HsnCode/HsnController.php
@@ -18,21 +18,40 @@ class HsnController extends Controller
             $length = strlen($searchTerm);
 
             // Validate the length of the HSN code in the request
-            if ($length % 2 == 0 && $length >= 2 && $length <= 8) {
+            if ($length == 1) {
                 $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
-                    ->whereRaw('CHAR_LENGTH(hsn_code) IN (?, ?, ?)', [$length, $length + 2, $length + 4])
+                    ->whereRaw('CHAR_LENGTH(hsn_code) = 2')
                     ->orderBy('hsn_code')
                     ->limit(8)
                     ->get();
-
-                return $this->success("Hsn List.", $searchResults, null, 200);
+            } elseif ($length <= 4) {
+                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
+                    ->whereRaw('CHAR_LENGTH(hsn_code) = 4')
+                    ->orderBy('hsn_code')
+                    ->limit(8)
+                    ->get();
+            } elseif ($length <= 6) {
+                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
+                    ->whereRaw('CHAR_LENGTH(hsn_code) = 6')
+                    ->orderBy('hsn_code')
+                    ->limit(8)
+                    ->get();
+            } elseif ($length <= 8) {
+                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
+                    ->whereRaw('CHAR_LENGTH(hsn_code) = 8')
+                    ->orderBy('hsn_code')
+                    ->limit(8)
+                    ->get();
             } else {
                 return $this->error('Invalid HSN code length in the request.', null, null, 400);
             }
+
+            return $this->success("Hsn List.", $searchResults, null, 200);
         } catch (\Exception $e) {
             return $this->error('Oops! Something Went Wrong.' . $e->getMessage(), null, null, 500);
         }
     }
+
 
 
 }
