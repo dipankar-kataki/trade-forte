5681ce414eed6c5863340f266dc0263b4bff0963
jay-ekodus <jsardar@ekodusinc.com>
2024-02-16 17:37:17 +0530

    invoice model update-98

--- app/Http/Controllers/HsnCode/HsnController.php
+++ app/Http/Controllers/HsnCode/HsnController.php
@@ -16,42 +16,29 @@ class HsnController extends Controller
         try {
             $searchTerm = $request->hsn;
             $length = strlen($searchTerm);
-
-            // Validate the length of the HSN code in the request
+    
             if ($length == 1) {
-                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
-                    ->whereRaw('CHAR_LENGTH(hsn_code) = 2')
-                    ->orderBy('hsn_code')
-                    ->limit(8)
-                    ->get();
+                $query = "SELECT * FROM hsn_table WHERE hsn_code LIKE ? AND CHAR_LENGTH(hsn_code) = 2 ORDER BY hsn_code LIMIT 8";
+                $searchResults = DB::select($query, [$searchTerm . '%']);
             } elseif ($length <= 4) {
-                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
-                    ->whereRaw('CHAR_LENGTH(hsn_code) = 4')
-                    ->orderBy('hsn_code')
-                    ->limit(8)
-                    ->get();
+                $query = "SELECT * FROM hsn_table WHERE hsn_code LIKE ? AND CHAR_LENGTH(hsn_code) = 4 ORDER BY hsn_code LIMIT 8";
+                $searchResults = DB::select($query, [$searchTerm . '%']);
             } elseif ($length <= 6) {
-                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
-                    ->whereRaw('CHAR_LENGTH(hsn_code) = 6')
-                    ->orderBy('hsn_code')
-                    ->limit(8)
-                    ->get();
+                $query = "SELECT * FROM hsn_table WHERE hsn_code LIKE ? AND CHAR_LENGTH(hsn_code) = 6 ORDER BY hsn_code LIMIT 8";
+                $searchResults = DB::select($query, [$searchTerm . '%']);
             } elseif ($length <= 8) {
-                $searchResults = HsnTable::where('hsn_code', 'LIKE', $searchTerm . '%')
-                    ->whereRaw('CHAR_LENGTH(hsn_code) = 8')
-                    ->orderBy('hsn_code')
-                    ->limit(8)
-                    ->get();
+                $query = "SELECT * FROM hsn_table WHERE hsn_code LIKE ? AND CHAR_LENGTH(hsn_code) = 8 ORDER BY hsn_code LIMIT 8";
+                $searchResults = DB::select($query, [$searchTerm . '%']);
             } else {
                 return $this->error('Invalid HSN code length in the request.', null, null, 400);
             }
-
+    
             return $this->success("Hsn List.", $searchResults, null, 200);
         } catch (\Exception $e) {
             return $this->error('Oops! Something Went Wrong.' . $e->getMessage(), null, null, 500);
         }
     }
-
+    
 
 
 }
