6dba138683cd458f6199485abbdbad49c74aed22
jay-ekodus <jsardar@ekodusinc.com>
2024-02-16 17:38:23 +0530

    invoice model update-99

--- app/Http/Controllers/HsnCode/HsnController.php
+++ app/Http/Controllers/HsnCode/HsnController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\HsnTable;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class HsnController extends Controller
