756b705fec202fc5f9a3e57584e84dddd405cb9d
jay-ekodus <jsardar@ekodusinc.com>
2024-02-19 15:43:14 +0530

    updated con bank-72

--- app/Http/Controllers/ShippingAddress/ShippingAddressController.php
+++ app/Http/Controllers/ShippingAddress/ShippingAddressController.php
@@ -23,44 +23,33 @@ class ShippingAddressController extends Controller
             if (!$user_id) {
                 throw new \Exception('User not authenticated.');
             }
-
             $addresses = $request->addresses;
-
             // Start a database transaction
             DB::beginTransaction();
-
             foreach ($addresses as $address) {
                 // Validate the address data
                 $validator = Validator::make($address, ShippingAddress::createRule());
                 $data = $validator->validated();
-
                 // Set the user ID
                 $data['users_id'] = $user_id;
-
                 // Create the shipping address
                 $shipping = ShippingAddress::create($data);
-
                 // Log the action
                 $this->createLog($user_id, "Shipping address added.", "shipping", $shipping->id);
             }
-
             // Commit the transaction
             DB::commit();
-
             // Return a success response
             return $this->success("Shipping details created successfully!", null, null, 201);
         } catch (\Exception $e) {
             // Rollback the transaction in case of an exception
             DB::rollBack();
-
             // Log the exception for debugging purposes
             \Log::error('Error in shipping address creation: ' . $e->getMessage());
-
             // Return an error response
             return $this->error('Oops! Something went wrong. ' . $e->getMessage(), null, null, 500);
         }
     }
-
 
     public function index(Request $request)
     {
@@ -92,9 +81,24 @@ class ShippingAddressController extends Controller
         } else {
             try {
                 DB::beginTransaction();
-                ShippingAddress::where('id', $request->id)->update($request->all());
+                foreach ($request->items as $item) {
+                    // $validator = Validator::make($request->all(), InvoiceItem::updateRule());
+                    // if ($validator->fails()) {
+                    //     return $this->error('Oops!' . $validator->errors()->first(), null, null, 400);
+                    // }
+                    $invItem = ShippingAddress::where('id', $request->id)->first();
+
+                    // Loop through attributes dynamically and update only if $item value is not null
+
+                    foreach ($item as $key => $value) {
+                        if ($value !== null) {
+                            $invItem->$key = $value;
+                        }
+                    }
+                    $invItem->save(); // Save the changes
+                }
                 $user_id = Auth::id();
-                $this->createLog($user_id, "Shipping address updated.", "shipping", $request->id);
+                $this->createLog($user_id, "Shipping address updated.", "shipping", null);
                 DB::commit();
                 return $this->success("Shipping updated successfully.", null, null, 200);
             } catch (\Exception $e) {
