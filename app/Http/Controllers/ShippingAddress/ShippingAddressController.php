167c7e61846c283d2a2718c052b618ddf4e3d74e
jay-ekodus <jsardar@ekodusinc.com>
2024-02-19 14:36:27 +0530

    updated con bank-64

--- app/Http/Controllers/ShippingAddress/ShippingAddressController.php
+++ app/Http/Controllers/ShippingAddress/ShippingAddressController.php
@@ -23,44 +23,44 @@ class ShippingAddressController extends Controller
             if (!$user_id) {
                 throw new \Exception('User not authenticated.');
             }
-    
+
             $addresses = $request->addresses;
-    
+
             // Start a database transaction
             DB::beginTransaction();
-    
+
             foreach ($addresses as $address) {
                 // Validate the address data
                 $validator = Validator::make($address, ShippingAddress::createRule());
                 $data = $validator->validated();
-    
+
                 // Set the user ID
                 $data['users_id'] = $user_id;
-    
+
                 // Create the shipping address
                 $shipping = ShippingAddress::create($data);
-    
+
                 // Log the action
                 $this->createLog($user_id, "Shipping address added.", "shipping", $shipping->id);
             }
-    
+
             // Commit the transaction
             DB::commit();
-    
+
             // Return a success response
             return $this->success("Shipping details created successfully!", null, null, 201);
         } catch (\Exception $e) {
             // Rollback the transaction in case of an exception
             DB::rollBack();
-    
+
             // Log the exception for debugging purposes
             \Log::error('Error in shipping address creation: ' . $e->getMessage());
-    
+
             // Return an error response
             return $this->error('Oops! Something went wrong. ' . $e->getMessage(), null, null, 500);
         }
     }
-    
+
 
     public function index(Request $request)
     {
@@ -77,7 +77,7 @@ class ShippingAddressController extends Controller
         try {
             $shipping = ShippingAddress::where(function ($query) use ($request) {
                 $query->where('id', $request->id)
-                    ->orWhere('consignee_id', $request->id);
+                    ->orWhere('exporter_id', $request->id);
             })->get();
             if (!$shipping) {
                 return $this->error("Shipping details not found.", null, null, 404);
