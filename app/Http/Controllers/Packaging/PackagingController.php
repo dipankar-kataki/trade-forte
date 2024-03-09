b6813b661879a821496b8f6a11208ee7306a52d4
jay-ekodus <jsardar@ekodusinc.com>
2024-03-09 11:17:19 +0530

    addede lorry-12

--- app/Http/Controllers/Packaging/PackagingController.php
+++ app/Http/Controllers/Packaging/PackagingController.php
@@ -67,7 +67,7 @@ class PackagingController extends Controller
 
             // Update invoice details with additional information
             // dd($packagingDetails);
-            $invoice = InvoiceDetail::where("id", $request->invoice_details_id)->first();
+            $invoice = InvoiceDetail::where("id",$packagingDetails->invoice_details_id)->first();
             $invoice->with_letter_head = $request->packaging_details['with_letter_head'];
             $invoice->save();
             $invoice->lazy;
