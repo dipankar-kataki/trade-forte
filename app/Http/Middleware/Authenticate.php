b004c9ae18a16b4e3530ae4832360f45547bd248
jay-ekodus <jsardar@ekodusinc.com>
2024-03-11 11:36:25 +0530

    added login redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!$request->expectsJson()) {
+            return 'http://localhost:127.0.0.1/login';
         }
     }
+
 }
